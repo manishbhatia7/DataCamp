99906e98e43aba058092b1af5ab32438c37062b2
Manish Bhatia <manish.bhatia1979@gmail.com>
2024-10-19 12:13:32 +0530

    Code Blocks

--- Useful Codeblocks.py
+++ Useful Codeblocks.py
@@ -1,4 +1,7 @@
 # Check for Multicollinearity
+from sklearn.model_selection import GridSearchCV
+
+
 def correlation(dataset,threshold):
     col_corr=set()
     corr_matrix=dataset.corr()
@@ -24,3 +27,17 @@ show()
 
 #ROC AUC
 roc_auc_score(y_test,y_pred_probs)
+
+
+#Grid Search CV
+kf=KFold(n_splits=5,random_state=10,shuffle=True)
+param_grid={"alpha":np.arange(0.0001,1,10),"solver":["sag","lsqr"]}
+ridge=Ridge()
+ridge_cv=GridSearchCV(ridge,param_grid,cv=kf)
+
+
+#Create the parameter space
+params = {"penalty": ["l1", "l2"],
+         "tol": np.linspace(0.0001, 1.0, 50),
+         "C": np.linspace(0.1,1.0,50),
+         "class_weight": ["balanced", {0:0.8, 1:0.2}]}
