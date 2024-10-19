f8d770beb9e85a54ada95cda48e0c281643cada4
Manish Bhatia <manish.bhatia1979@gmail.com>
2024-10-20 00:43:03 +0530

    Code Blocks

--- Useful Codeblocks.py
+++ Useful Codeblocks.py
@@ -81,3 +81,21 @@ for name,model in models.items():
     test_score=model.score(X_test,y_test)
     print(name,test_score)
 
+#Preprocessing Model
+# Create steps
+steps = [("imp_mean",SimpleImputer()),
+         ("scaler", StandardScaler()),
+         ("logreg", LogisticRegression())]
+
+# Set up pipeline
+pipeline = Pipeline(steps)
+params = {"logreg__solver": ["newton-cg", "saga", "lbfgs"],
+         "logreg__C": np.linspace(0.001, 1.0, 10)}
+
+# Create the GridSearchCV object
+tuning = GridSearchCV(pipeline, param_grid=params)
+tuning.fit(X_train, y_train)
+y_pred = tuning.predict(X_test)
+
+# Compute and print performance
+print("Tuned Logistic Regression Parameters: {}, Accuracy: {}".format(tuning.best_params_,tuning.score(X_test,y_test)))
